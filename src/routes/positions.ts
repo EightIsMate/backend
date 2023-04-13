1bdde6e8ace159468fbe8309a7c3063731e69f34
ImCookiieZ <53428372+ImCookiieZ@users.noreply.github.com>
2023-04-13 17:13:26 +0200

    add try and catch to positions route

--- src/routes/positions.ts
+++ src/routes/positions.ts
@@ -6,15 +6,20 @@ import { position_storing } from '../modules/positions';
 const router : Router = Router();
 
 router.post('/positions', async (req: Request, res: Response) => {
-    const position_horizontal: string = req.body.position_horizontal || null
-    const position_vertical: string = req.body.position_vertical || null
-    
-    if (position_horizontal == null || position_vertical == null) {
-        res.status(400).send(`${position_horizontal == null ? "'position_horizontal'" : "'position_vertical'"} positions are missing`);
-        return
+    try {
+        const position_horizontal: string = req.body.position_horizontal || null;
+        const position_vertical: string = req.body.position_vertical || null;
+        
+        if (position_horizontal == null || position_vertical == null) {
+            res.status(400).send(`${position_horizontal == null ? "'position_horizontal'" : "'position_vertical'"} positions are missing`);
+            return;
+        }
+        await position_storing(position_horizontal, position_vertical);
+        res.status(201).send();
+    } catch (error) {
+        console.error(error);
+        res.status(500).send(error);
     }
-    await position_storing(position_horizontal, position_vertical);
-    res.status(201).send();
 });
 
 export default router;
